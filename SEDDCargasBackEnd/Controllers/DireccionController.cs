af2f72a206365c9937e16ead983215aa7433bc65
Abrancill0 <38171762+Abrancill0@users.noreply.github.com>
2022-01-15 23:45:39 -0600

    Apis de carga parte 1

---
+++ SEDDCargasBackEnd/Controllers/DireccionController.cs
@@ -0,0 +1,96 @@
+﻿using Newtonsoft.Json.Linq;
+using SEDDCargasBackEnd.Clases;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SEDDCargasBackEnd.Controllers
+{
+    public class DireccionController : ApiController
+    {
+        public class ParametorsEntrada
+        {
+            public string Arreglo { get; set; }
+
+        }
+
+        public JObject Post(ParametorsEntrada Datos)
+        {
+
+            try
+            {
+                string Mensaje = "";
+                int Estatus = 0;
+
+                string Arreglover = Datos.Arreglo;
+
+                string[] ArregloFinal = Arreglover.Split('{');
+
+                for (int i = 1; i < ArregloFinal.Length; i++)
+                {
+                    string ArregloSimple = ArregloFinal[i];
+
+                    string EliminaParte1 = ArregloSimple.Replace("{", "");
+                    string EliminaParte2 = EliminaParte1.Replace("},", "");
+                    string EliminaParte3 = EliminaParte2.Replace("}", "");
+
+                    string[] Valores = EliminaParte3.Split(',');
+
+                    string NombreEmpresa = Convert.ToString(Valores[0]);
+                    string Direccion = Convert.ToString(Valores[1]);
+
+                    SqlCommand comando2 = new SqlCommand("Cargas.AltaDireccion");
+                    comando2.CommandType = CommandType.StoredProcedure;
+
+                    //Declaracion de parametros 
+                    comando2.Parameters.Add("@NombreEmpresa", SqlDbType.VarChar);
+                    comando2.Parameters.Add("@Direccion", SqlDbType.VarChar);
+
+                    //Asignacion de valores a parametros
+                    comando2.Parameters["@NombreEmpresa"].Value = NombreEmpresa;
+                    comando2.Parameters["@Direccion"].Value = @Direccion;
+
+                    comando2.Connection = new SqlConnection(VariablesGlobales.CadenaConexion);
+                    comando2.CommandTimeout = 0;
+                    comando2.Connection.Open();
+
+                    DataTable DT2 = new DataTable();
+                    SqlDataAdapter DA2 = new SqlDataAdapter(comando2);
+                    comando2.Connection.Close();
+                    DA2.Fill(DT2);
+
+                     Mensaje = "OK";
+                     Estatus = 1;
+
+                }
+                
+                JObject Resultado = JObject.FromObject(new
+                {
+                    mensaje = Mensaje,
+                    estatus = Estatus,
+                });
+
+                return Resultado;
+
+            }
+            catch (Exception ex)
+            {
+
+                JObject Resultado = JObject.FromObject(new
+                {
+                    mensaje = ex.ToString(),
+                    estatus = 0,
+
+                });
+
+                return Resultado;
+            }
+
+        }
+    }
+}
